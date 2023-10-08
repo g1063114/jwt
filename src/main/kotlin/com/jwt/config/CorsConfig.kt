d8a93b8eb01f26e752bbbd9059edccb100a6c69e
jinho <g1063114@naver.com>
2023-10-08 19:44:56 +0900

    feat: security filter 등록

--- src/main/kotlin/com/jwt/config/CorsConfig.kt
+++ src/main/kotlin/com/jwt/config/CorsConfig.kt
@@ -1,4 +1,23 @@
 package com.jwt.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
 class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true  // 서버 응답할 때 json을 js에서 처리할 수 있게 할지 설정
+        config.addAllowedOrigin("*")    // 모든 ip에 응답을 허용
+        config.addAllowedHeader("*")    // 모든 header에 응답을 허용
+        config.addAllowedMethod("*")    // 모든 http method에 응답을 허용
+        source.registerCorsConfiguration("/api/**", config)
+        return CorsFilter(source)
+    }
 }
--- src/main/kotlin/com/jwt/config/SecurityConfig.kt
+++ src/main/kotlin/com/jwt/config/SecurityConfig.kt
@@ -1,15 +1,23 @@
 package com.jwt.config
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.web.filter.CorsFilter
 
-class SecurityConfig: WebSecurityConfigurerAdapter() {
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    val corsFilter: CorsFilter
+): WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http.csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
+            .addFilter(corsFilter)
             .formLogin().disable()
             .httpBasic().disable()
             .authorizeRequests()
